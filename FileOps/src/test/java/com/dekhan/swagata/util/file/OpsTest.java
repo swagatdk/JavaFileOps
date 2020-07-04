3d4f4fc2911efb7e62cf20f1830999fcb4181ac3
Swagata De Khan <swagata.dekhan@yahoo.com>
2020-07-04 20:17:57 +0530

    Fixed Security Issue in Log4j2-core

--- FileOps/src/test/java/com/dekhan/swagata/util/file/OpsTest.java
+++ FileOps/src/test/java/com/dekhan/swagata/util/file/OpsTest.java
@@ -17,61 +17,61 @@ import org.junit.Test;
  * Unit test for simple Ops.
  */
 public class OpsTest {
-    
-    private static final Logger log = LogManager.getLogger(com.dekhan.swagata.util.file.OpsTest.class);
+
+    private static Logger log = LogManager.getLogger(com.dekhan.swagata.util.file.OpsTest.class);
     private static Ops ops;
-    
+
     private static String dir = ".";
-    private static String copyDir = "D:\\temp\\test\\copy\\";    
+    private static String copyDir = "D:\\temp\\test\\copy\\";
     private static String pattern = "*.prefs";
     private static boolean isGlob = true;
-    
+
     @BeforeClass
     public static void initOps() {
-        ops = new Ops();
-        log.info("Main Class Init complete.");
+	ops = new Ops();
+	log.info("Main Class Init complete.");
     }
- 
+
     @Before
     public void beforeEachTest() {
-	log.info("Before Test");        
+	log.info("Before Test");
     }
- 
+
     @After
     public void afterEachTest() {
-	log.info("After Test");    
-    }    
-    
+	log.info("After Test");
+    }
+
     @Test
-    public void testListMatchedAbsPath()  {
+    public void testListMatchedAbsPath() {
 	List<String> result = new ArrayList<String>();
 	try {
 	    result = ops.listMatchedAbsPath(dir, pattern, isGlob);
 	} catch (IOException e) {
 	    log.error(e.getMessage(), e);
 	}
-        assertEquals(3, result.size());
+	assertEquals(3, result.size());
     }
-    
+
     @Test
-    public void testListMatchedRelPath()  {
+    public void testListMatchedRelPath() {
 	List<String> result = new ArrayList<String>();
 	try {
 	    result = ops.listMatchedRelPath(dir, pattern, isGlob);
 	} catch (IOException e) {
 	    log.error(e.getMessage(), e);
 	}
-        assertEquals(3, result.size());
-    } 
-    
+	assertEquals(3, result.size());
+    }
+
     @Test
     public void testCopyMatchedFiles() {
-        List<String> result = new ArrayList<String>();
+	List<String> result = new ArrayList<String>();
 	try {
 	    result = ops.copyMatchedFiles(dir, copyDir, pattern, isGlob);
 	} catch (IOException e) {
 	    log.error(e.getMessage(), e);
-	} 
-        assertEquals(3, result.size());
+	}
+	assertEquals(3, result.size());
     }
 }
